401028689184c4864ee367d9b9780871fe7029df
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2024-05-30 13:06:11 +0200

    Update gbc_palettes.asm

--- data/gbc_palettes.asm
+++ data/gbc_palettes.asm
@@ -68,45 +68,45 @@ GBCBasePalettes:
 
 	; PAL_LOGO1
 IF DEF(_BLUE)
-	RGB 31, 31, 31	;white bg
+	RGB 31, 29, 31	;white bg
 	RGB 31, 31,  0	;unused yellow logo text
 	RGB 21,  0,  4	;unused on title screen
-	RGB  3,  3, 23	;version subtitle text color
+	RGB  3,  2,  2	;version subtitle text color
 ENDC
 IF DEF(_RED)
-	RGB 31, 31, 31	;white bg
+	RGB 31, 29, 31	;white bg
 	RGB 31, 31,  0	;unused yellow logo text
 	RGB 17, 23, 10	;unused on title screen
-	RGB 23,  3,  3	;version subtitle text color
+	RGB  3,  2,  2	;version subtitle text color
 ENDC
 IF DEF(_GREEN)
-	RGB 31, 31, 31	;white bg
+	RGB 31, 29, 31	;white bg
 	RGB 31, 31,  0	;unused yellow logo text
 	RGB 17, 23, 10	;unused on title screen
-	RGB  3, 23,  3	;version subtitle text color
+	RGB  3,  2,  2	;version subtitle text color
 ENDC
 
 	; PAL_LOGO2
 IF (DEF(_RED) && DEF(_JPLOGO))
-	RGB 31, 31, 31	;white bg
+	RGB 31, 29, 31	;white bg
 	RGB 31, 31,  0	;unused yellow logo text
 	RGB  3,  3, 23	;"pocket monsters" logo text color
-	RGB 23,  3,  3	;japanese logo text color
+	RGB  3,  2,  2	;japanese logo text color
 ELIF (DEF(_GREEN) && DEF(_JPLOGO))
-	RGB 31, 31, 31	;white bg
+	RGB 31, 29, 31	;white bg
 	RGB 31, 31,  0	;unused yellow logo text
 	RGB  3,  3, 23	;"pocket monsters" logo text color
-	RGB  3, 23,  3	;japanese logo text color
+	RGB  3,  2,  2	;japanese logo text color
 ELIF (DEF(_BLUE) && DEF(_JPLOGO))
-	RGB 31, 31, 31	;white bg
+	RGB 31, 29, 31	;white bg
 	RGB 31, 31,  0	;unused yellow logo text
-	RGB 31,  15, 0	;"pocket monsters" logo text color
-	RGB  3,  3, 23	;japanese logo text color
+	RGB 31, 15,  0	;"pocket monsters" logo text color
+	RGB  3,  2,  2	;japanese logo text color
 ELSE
-	RGB 31, 31, 31	;white bg
+	RGB 31, 29, 31	;white bg
 	RGB 31, 31,  0	;yellow logo text
 	RGB  7,  7, 25	;blue logo text shadow
-	RGB  0,  0, 17	;blue logo text outline
+	RGB  3,  2,  2	;blue logo text outline
 ENDC
 	RGB 31,29,31 ; PAL_0F
 	RGB 24,20,30
@@ -166,74 +166,74 @@ ENDC
 ;gbcnote - retouched all the slot palettes to match the red/blue coloring
 	; PAL_SLOTS1
 IF DEF(_GREEN)
-	RGB 31, 31, 31	;reel background
+	RGB 31, 29, 31	;reel background
 	RGB 21, 12, 15	;reel accents
 	RGB 16,  0,  0	;"7" fill color
-	RGB  3,  3,  3	;reel outline
+	RGB  3,  2,  2	;reel outline
 ELSE
-	RGB 31, 31, 31	;reel background
+	RGB 31, 29, 31	;reel background
 	RGB 21, 12, 15	;reel accents
 	RGB 21, 14,  0	;"7" fill color
-	RGB  3,  3,  3	;reel outline
+	RGB  3,  2,  2	;reel outline
 ENDC
 	; PAL_SLOTS2
 IF DEF(_RED)
-	RGB 31, 31, 31	;"3" icon fill
+	RGB 31, 29, 31	;"3" icon fill
 	RGB 31, 31,  0	;"3" icon shape color
 	RGB 20,  8, 15	;"3" icon background color
-	RGB  3,  3,  3	;"3" icon outline
+	RGB  3,  2,  2	;"3" icon outline
 ENDC
 IF DEF(_BLUE)
-	RGB 31, 31, 31	;"3" icon fill
+	RGB 31, 29, 31	;"3" icon fill
 	RGB 31, 31,  0	;"3" icon shape color
 	RGB  9,  5, 30	;"3" icon background color
-	RGB  3,  3,  3	;"3" icon outline
+	RGB  3,  2,  2	;"3" icon outline
 ENDC
 IF DEF(_GREEN)
-	RGB 31, 31, 31	;"3" icon fill
+	RGB 31, 29, 31	;"3" icon fill
 	RGB 31, 31,  0	;"3" icon shape color
 	RGB 12, 21,  7	;"3" icon background color
-	RGB  3,  3,  3	;"3" icon outline
+	RGB  3,  2,  2	;"3" icon outline
 ENDC
 
 	; PAL_SLOTS3
 IF DEF(_RED)
-	RGB 31, 31, 31	;"2" icon fill
+	RGB 31, 29, 31	;"2" icon fill
 	RGB  3, 31,  9	;"2" icon shape color
 	RGB 20,  8, 15	;"2" icon background color
-	RGB  3,  3,  3	;"2" icon outline
+	RGB  3,  2,  2	;"2" icon outline
 ENDC
 IF DEF(_BLUE)
-	RGB 31, 31, 31	;"2" icon fill
+	RGB 31, 29, 31	;"2" icon fill
 	RGB  3, 31,  9	;"2" icon shape color
 	RGB  9,  5, 30	;"2" icon background color
-	RGB  3,  3,  3	;"2" icon outline
+	RGB  3,  2,  2	;"2" icon outline
 ENDC
 IF DEF(_GREEN)
-	RGB 31, 31, 31	;"2" icon fill
+	RGB 31, 29, 31	;"2" icon fill
 	RGB 20,  8, 15	;"2" icon shape color
 	RGB 12, 21,  7	;"2" icon background color
-	RGB  3,  3,  3	;"2" icon outline
+	RGB  3,  2,  2	;"2" icon outline
 ENDC
 
 	; PAL_SLOTS4
 IF DEF(_RED)
-	RGB 31, 31, 31	;"1" icon fill
+	RGB 31, 29, 31	;"1" icon fill
 	RGB  9,  5, 30	;"1" icon shape color
 	RGB 20,  8, 15	;"1" icon background color
-	RGB  3,  3,  3	;"1" icon outline
+	RGB  3,  2,  2	;"1" icon outline
 ENDC
 IF DEF(_BLUE)
-	RGB 31, 31, 31	;"1" icon fill
+	RGB 31, 29, 31	;"1" icon fill
 	RGB 20,  8, 15	;"1" icon shape color
 	RGB  9,  5, 30	;"1" icon background color
-	RGB  3,  3,  3	;"1" icon outline
+	RGB  3,  2,  2	;"1" icon outline
 ENDC
 IF DEF(_GREEN)
-	RGB 31, 31, 31	;"1" icon fill
+	RGB 31, 29, 31	;"1" icon fill
 	RGB  9,  5, 30	;"1" icon shape color
 	RGB 12, 21,  7	;"1" icon background color
-	RGB  3,  3,  3	;"1" icon outline
+	RGB  3,  2,  2	;"1" icon outline
 ENDC
 
 	RGB 31,29,31 ; PAL_BLACK
@@ -272,32 +272,32 @@ ENDC
 	RGB 3,2,2
 
 	; PAL_25
-	RGB 31, 31, 31
+	RGB 31, 29, 31
 	RGB 31, 31,  0
 	RGB 11, 23, 31
-	RGB  3,  3,  3
+	RGB  3,  2,  2
 
 	; PAL_26
-	RGB 31, 31, 31
+	RGB 31, 29, 31
 	RGB 31, 18,  0
 	RGB 19,  7,  1
-	RGB  3,  3,  3
+	RGB  3,  2,  2
 
 	; PAL_27
-	RGB 31, 31, 31
+	RGB 31, 29, 31
 	RGB  9,  9,  9
 	RGB 31, 21,  0
-	RGB  3,  3,  3
+	RGB  3,  2,  2
 
 	; PAL_BW	;joenote - adding a black & white palette just for GBC
-	RGB 31, 31, 31
-	RGB 31, 31, 31
+	RGB 31, 29, 31
+	RGB 31, 29, 31
 	RGB  3,  3,  3
-	RGB  3,  3,  3
+	RGB  3,  2,  2
 
 	; PAL_UBALL	;joenote - adding a pal just for ultra balls on GBC
-	RGB 31, 31, 31
+	RGB 31, 29, 31
 	RGB 24, 24, 24
 	RGB  8,  8,  8
-	RGB  3,  3,  3
+	RGB  3,  2,  2
 	
